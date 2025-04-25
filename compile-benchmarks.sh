4270ad254d58576232b181ad43584a34034d45ff
Radek Micek <radek.micek@gmail.com>
2025-04-25 13:32:09 +0200

    Benchmark C3 HashMap

--- compile-benchmarks.sh
+++ compile-benchmarks.sh
@@ -8,4 +8,5 @@ mkdir -p "$build_dir"
 cd "$build_dir"
 
 c3c compile -O2 --libdir .. --lib btree ../benchmarks/bench.c3 -o bench-c3
+c3c compile -O2 ../benchmarks/bench_hash_map.c3 -o bench_hash_map-c3
 rustc -C opt-level=2 ../benchmarks/bench.rs -o bench-rust
